7a3051c53e44d74d0ee1b035afe2ba7bda0ffedf
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-13 16:09:24 +0000

    creates Manager class and tests all passing

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ManagerTest {
+
+    private Manager manager;
+
+    @Before
+    public void before() {
+        manager = new Manager("Phil", "GG679394T", 80000, "Technical");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Phil", manager.getName());
+    }
+
+    @Test
+    public void canGetNiNumber() {
+        assertEquals("GG679394T", manager.getNiNumber());
+    }
+
+    @Test
+    public void canGetSalary() {
+        assertEquals(80000, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetDeptName() {
+        assertEquals("Technical", manager.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        manager.raiseSalary(1000);
+        assertEquals(81000, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetBonus() {
+        assertEquals(800, manager.getBonus(), 0.01);
+    }
+
+}
